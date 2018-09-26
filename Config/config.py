6671c064272a77a97e74caedd0206a27f95e8ffa
Cresko Yulvir Puyana Moron <yulvir92@gmail.com>
2018-09-26 20:22:31 +0200

    Upload scritp

---
+++ Config/config.py
@@ -0,0 +1,30 @@
+START_DATE = "2018-08-25"
+END_DATE = "2018-10-05"
+
+
+translate_weekday = {
+    "Monday" : "Lunes",
+    "Tuesday": "Martes",
+    "Wednesday": "Miercoles",
+    "Thursday": "Jueves",
+    "Friday": "Viernes",
+    "Saturday": "Sabado",
+    "Sunday": "Domingo"
+}
+
+translate_months = {
+    "January": "Enero",
+    "February": "Febrero",
+    "March": "Marzo",
+    "April": "Abril",
+    "May": "Mayo",
+    "June": "Junio",
+    "July": "Julio",
+    "August": "Agosto",
+    "September": "Septiembre",
+    "October": "Octubre",
+    "November": "Noviembre",
+    "December": "Diciembre"
+}
+
+
